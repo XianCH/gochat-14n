b8445204367d5991592a41a2fca99f16c4c09a7e
XianCH <1160698903@qq.com>
2023-11-15 16:15:40 +0800

    global

---
+++ global/global.go
@@ -0,0 +1,20 @@
+package global
+
+import (
+	"github.com/x14n/go-chat-x14n/config"
+	"go.uber.org/zap"
+	"gorm.io/gorm"
+)
+
+const (
+	ConfigFile     = "./config.yml"
+	YYYYMMDD       = "2006-01-02"
+	YYYYMMDDHHIISS = "2006-01-02 15:04:05"
+	StaticFilePath = "web/static/file/"
+)
+
+var (
+	GLogger *zap.Logger
+	_DB     *gorm.DB
+	GConfig config.ServerConfig
+)
