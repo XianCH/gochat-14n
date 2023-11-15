0260ecd44b42012707d42bf5903918cf54c0f4e1
XianCH <1160698903@qq.com>
2023-11-15 16:14:48 +0800

    constant

---
+++ common/constant/constant.go
@@ -0,0 +1,21 @@
+package constant
+
+const (
+	HEAT_BEAT = "heatbeat"
+	PING      = "ping"
+	PONG      = "pong"
+
+	MESSAGE_TYPE_TOUSER  = 1
+	MESSAGE_TYPE_TOGROUP = 2
+
+	//消息内容类型
+	TEXT  = 1
+	FILE  = 2
+	IMAGE = 3
+	AUDIO = 4
+	VIDIO = 5
+
+	//消息队列类型
+	GO_CHANEL = 1
+	KAFKA     = 2
+)
