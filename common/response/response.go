86034c0d6ce9014cd530fb5fef8aaae6e263565a
XianCH <1160698903@qq.com>
2023-11-21 17:51:36 +0800

    response

---
+++ common/response/response.go
@@ -0,0 +1,49 @@
+package response
+
+type ResponseMessage struct {
+	Code int    `json:"code"`
+	Msg  string `json:"Msg"`
+	Data any    `json:"Data"`
+}
+
+func Success() *ResponseMessage {
+	return &ResponseMessage{
+		Code: 0,
+	}
+}
+
+func SuccessWithMsg(msg string) *ResponseMessage {
+	return &ResponseMessage{
+		Code: 0,
+		Msg:  msg,
+	}
+}
+
+func SuccessWithMsgData(msg string, data any) *ResponseMessage {
+	return &ResponseMessage{
+		Code: 0,
+		Msg:  msg,
+		Data: data,
+	}
+}
+
+func Fail() *ResponseMessage {
+	return &ResponseMessage{
+		Code: -1,
+	}
+}
+
+func FailWithMsg(msg string) *ResponseMessage {
+	return &ResponseMessage{
+		Code: -1,
+		Msg:  msg,
+	}
+}
+
+func FailWithMsgData(msg string, data any) *ResponseMessage {
+	return &ResponseMessage{
+		Code: -1,
+		Msg:  msg,
+		Data: data,
+	}
+}
