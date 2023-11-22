c02e725d6f7d9e4b816b3ae8d75dc61dca4e44c2
XianCH <1160698903@qq.com>
2023-11-22 18:33:52 +0800

    global

--- global/global.go
+++ global/global.go
@@ -1,13 +1,14 @@
 package global
 
 import (
+	"github.com/redis/go-redis/v9"
 	"github.com/x14n/go-chat-x14n/config"
 	"go.uber.org/zap"
 	"gorm.io/gorm"
 )
 
 const (
-	ConfigFile     = "./config.yml"
+	ConfigFile     = "../config.yml"
 	YYYYMMDD       = "2006-01-02"
 	YYYYMMDDHHIISS = "2006-01-02 15:04:05"
 	StaticFilePath = "web/static/file/"
@@ -17,4 +18,5 @@ var (
 	GLogger *zap.Logger
 	DB      *gorm.DB
 	GConfig config.ServerConfig
+	GRedis  *redis.Client
 )
