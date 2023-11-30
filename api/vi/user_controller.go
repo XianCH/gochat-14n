4bd606f9ba51162487cb0b4356c33a2985a5517b
XianCH <1160698903@qq.com>
2023-11-30 20:14:28 +0800

    user_controller

--- api/vi/user_controller.go
+++ api/vi/user_controller.go
@@ -4,12 +4,18 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/x14n/go-chat-x14n/common/request"
 	"github.com/x14n/go-chat-x14n/common/response"
 	"github.com/x14n/go-chat-x14n/global"
 	"github.com/x14n/go-chat-x14n/model"
 	"github.com/x14n/go-chat-x14n/service"
 	"go.uber.org/zap"
 )
+
+//todo:
+//1.密码加密
+//2.token认证
+//3.分布式id方案
 
 func Login(c *gin.Context) {
 	var user model.User
@@ -36,6 +42,19 @@ func Register(c *gin.Context) {
 	c.JSON(http.StatusOK, response.SuccessWithMsg("注册成功"))
 }
 
-func UpdataAvatar(c *gin.Context) {
+func GetUserDetails(c *gin.Context) {
+	uuid := c.Param("uuid")
+	u := service.UserService.GetUserDetails(uuid)
+	c.JSON(http.StatusOK, response.SuccessWithData(u))
+}
 
+func AddFriend(c *gin.Context) {
+	var userFriendRequest request.FriendRequest
+	c.ShouldBindJSON(&userFriendRequest)
+
+	err := service.UserService.AddFriend(&userFriendRequest)
+	if err != nil {
+		c.JSON(http.StatusOK, response.FailWithMsg(err.Error()))
+	}
+	c.JSON(http.StatusOK, response.Success())
 }
