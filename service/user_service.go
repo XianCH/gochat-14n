cc831965ea42dd144aab9af11181db8329eaf851
XianCH <1160698903@qq.com>
2023-11-30 20:15:51 +0800

    user_service

--- service/user_service.go
+++ service/user_service.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 
 	"github.com/google/uuid"
+	"github.com/x14n/go-chat-x14n/common/request"
 	"github.com/x14n/go-chat-x14n/global"
 	"github.com/x14n/go-chat-x14n/model"
 	"go.uber.org/zap"
@@ -13,6 +14,7 @@ type userService struct{}
 
 var UserService = new(userService)
 
+// 获取用户信息
 func (u *userService) GetUserDetails(uuid string) model.User {
 	var queryUser *model.User
 	db := global.DB
@@ -20,6 +22,7 @@ func (u *userService) GetUserDetails(uuid string) model.User {
 	return *queryUser
 }
 
+// 登录service
 func (u *userService) Login(user *model.User) bool {
 	db := global.DB
 	var queryUser *model.User
@@ -32,6 +35,7 @@ func (u *userService) Login(user *model.User) bool {
 	return user.Password == queryUser.Password
 }
 
+// 注册service
 func (u *userService) Register(user *model.User) error {
 	db := global.DB
 	var userCount int64
@@ -41,5 +45,47 @@ func (u *userService) Register(user *model.User) error {
 	}
 	user.Uuid = uuid.New().String()
 	db.Create(&user)
+	return nil
+}
+
+func (u *userService) ChangeUserAvatar(avatar string, uuid string) error {
+	var queryUser *model.User
+	db := global.DB
+	db.First(&queryUser, "uuid = ?", uuid)
+	if NULL_ID == queryUser.Id {
+		return errors.New("用户不存在")
+	}
+	db.Model(&queryUser).Update("avatar", avatar)
+	return nil
+}
+
+// todo :好友是否同意申请
+func (u *userService) AddFriend(userFriendRequest *request.FriendRequest) error {
+	var queryUser *model.User
+	db := global.DB
+	db.First(&queryUser, "uuid", userFriendRequest.Uuid)
+	if NULL_ID == queryUser.Id {
+		return errors.New("本用户不存在")
+	}
+
+	var friend *model.User
+	db.First(&friend, "username = ?", userFriendRequest.FriendName)
+	if NULL_ID == friend.Id {
+		return errors.New("friend用户不存在")
+	}
+	userFriend := model.UserFriend{
+		UserId:   queryUser.Id,
+		FriendId: friend.Id,
+	}
+
+	var userFriendQuery *model.UserFriend
+	db.First(&userFriendQuery, "user_id = ? and firend_id =?", queryUser.Id, friend.Id)
+	if NULL_ID != userFriendQuery.ID {
+		return errors.New("用户已经是你好友")
+	}
+
+	db.AutoMigrate(&userFriend)
+	db.Save(&userFriend)
+	global.GLogger.Info("userFriend", zap.Any("userFriend", userFriend))
 	return nil
 }
